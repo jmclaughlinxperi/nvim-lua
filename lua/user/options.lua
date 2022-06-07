7a53bba81b192a84719a572960b8f4e0a1d6a4bb
James Mclaughlin <james.mclaughlin@xperi.com>
2022-06-07 16:28:52 +0100

    Initial commit.

---
+++ lua/user/options.lua
@@ -0,0 +1,23 @@
+vim.opt.backup = false
+vim.opt.clipboard = "unnamedplus"
+vim.opt.completeopt = { "menuone", "noselect" }
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.wrap = false
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.relativenumber = true
+vim.opt.nu = true
+vim.opt.hidden = true
+vim.opt.errorbells = false
+vim.opt.wrap = false
+vim.opt.swapfile = false
+vim.opt.incsearch = true
+vim.opt.termguicolors = true
+vim.opt.scrolloff = 8
+vim.opt.fileformat = 'unix'
+vim.opt.updatetime = 300
+
+vim.cmd 'autocmd CmdlineEnter /,? :set hlsearch'
+vim.cmd 'autocmd CmdlineLeave /,? :set nohlsearch'
