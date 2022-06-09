7dc3c951233f499eb3fdfa28f5b8e2bf2e802ac4
James Mclaughlin <james.mclaughlin@xperi.com>
2022-06-09 17:35:34 +0100

    Updated some keybindings.

--- lua/user/lsp/handlers.lua
+++ lua/user/lsp/handlers.lua
@@ -76,13 +76,7 @@ local function lsp_keymaps(bufnr)
 		'<cmd>lua vim.lsp.diagnostic.show_line_diagnostics({ border = "rounded" })<CR>',
 		opts
 	)
-	vim.api.nvim_buf_set_keymap(
-		bufnr,
-		"n",
-		"<C-j>",
-		'<cmd>lua vim.diagnostic.goto_next({ border = "rounded" })<CR>',
-		opts
-	)
+	vim.api.nvim_buf_set_keymap(bufnr, "n", "]d", '<cmd>lua vim.diagnostic.goto_next({ border = "rounded" })<CR>', opts)
 	vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
 	vim.cmd [[ command! Format execute 'lua vim.lsp.buf.formatting()' ]]
 end
